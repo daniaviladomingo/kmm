cd81b3dfd8820cd2fff7c68472a6774a8f271311
danielavila <daniaviladomingo@gmail.com>
2021-12-21 16:37:44 +0100

    change

--- shared/src/commonMain/kotlin/daniel/avila/ricknmortykmm/shared/features/favorites/mvi/CharactersFavoritesViewModel.kt
+++ shared/src/commonMain/kotlin/daniel/avila/ricknmortykmm/shared/features/favorites/mvi/CharactersFavoritesViewModel.kt
@@ -21,6 +21,7 @@ open class CharactersFavoritesViewModel :
 
     override fun handleEvent(event: CharactersFavoritesContract.Event) {
         when (event) {
+            // Hola
             CharactersFavoritesContract.Event.OnGetCharactersFavorites -> getCharactersFavorites()
         }
     }
